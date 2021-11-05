566794535692b142759d3a3540503435083adc8d
Mugi Fadilah <32443765+mugi789@users.noreply.github.com>
2021-11-05 20:59:20 +0700

    menambahkan menu

--- scan.sh
+++ scan.sh
@@ -9,11 +9,36 @@ cat << "EOF"
 EOF
 echo -ne '\n\r'
 url='https://api.macvendors.com/'
-echo -n -e " Masukkan mac address : \e[96m"
-read mac
-encode="${mac/:/-}"
-echo -e "\033[0;31m============================================\033[0m"
-scan=$url$encode
-hasil=$(curl -s $scan)
-echo -e " $hasil"
-echo -e "\033[0;31m============================================\033[0m"
+menu=("Manual" "Mass" "Quit")
+select opt in "${menu[@]}"
+do
+    case $opt in
+        "Manual")
+            echo -n -e ' Masukkan mac address : \e[96m'
+            read mac
+            encode="${mac/:/-}"
+            echo -e "\033[0;31m============================================\033[0m"
+            scan=$url$encode
+            hasil=$(curl -s $scan)
+            echo -e " $hasil"
+            echo -e "\033[0;31m============================================\033[0m"
+            ;;
+        "Mass")
+            echo -n -e ' Masukkan nama file : \e[96m'
+            read file
+            echo -e "\033[0;31m============================================\033[0m"
+            while IFS= read -r line;
+            do
+            encode="${line/:/-}"
+            scan=$url$encode
+            hasil=$(curl -s $scan)
+            echo -e "$line - $hasil"
+            done < $file
+            echo -e "\033[0;31m============================================\033[0m"
+            ;;
+        "Quit")
+            break
+            ;;
+        *) echo "Opsi tidak valid";;
+    esac
+done
